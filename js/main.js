82e93ee28158b90e5686c70640bb56a8ab783ebc
AllanMorales94 <overlydedicated1994@gmail.com>
2023-07-27 14:57:43 -0700

    Excercise 2 Question

--- js/main.js
+++ js/main.js
@@ -13,3 +13,25 @@ let timerId = setTimeout(writeDing, 3000);
 // Solution : The problem with this code was that the function writeDing was being invoked inside the setTimeout method which did not allow the setTimout method to work properly.
 
 
+// Exercise 2
+
+// Javascript arrays have a built-in sort method that can take
+// a callback to tell it how to compare the things you want to sort.
+
+// Research the array sort method.
+
+// Write the sorting callback as a named function declaration
+
+// Write the callback function to provide to the sort method so that
+// the following code sorts the words from shortest to longest.
+
+const words = ['short', 'medium', 'delicious', 'nice', 'lengthy'];
+
+// The sort method sorts "in place", that is, it modifies the array
+words.sort(/* pass in a named callback here */);
+
+// Check that logging words now outputs
+// ["nice", "short", "medium", "lengthy", "delicious"]
+
+
+
