256ef01875d652ece66c1841b7ef9c2532804645
AllanMorales94 <overlydedicated1994@gmail.com>
2023-07-27 15:27:00 -0700

    Exercise 3

--- js/main.js
+++ js/main.js
@@ -38,3 +38,13 @@ words.sort(arrSort);
  console.log(words)
 
 
+// Exercise 3
+//
+// Filter the words array from above to create a new array
+// named longWords that includes only the words with 7 or more
+// characters
+
+const longWords = words.filter(/* write an anonymous inline function here */);
+
+// Check that logging longWords outputs
+// ["lengthy", "delicious"]
