a2861867ed241ae10b01c10ba0fffd94dcbabf09
AllanMorales94 <overlydedicated1994@gmail.com>
2023-07-27 14:56:52 -0700

    Solution for Exercise 1

--- js/main.js
+++ js/main.js
@@ -4,8 +4,12 @@
 // wait three seconds, then write "Ding!" to the console.  Instead, it writes
 // "Ding!" immediately.  Find the bug and fix it.
 
+
 function writeDing() {
   console.log('Ding!');
 }
 
-let timerId = setTimeout(writeDing(), 3000);
+let timerId = setTimeout(writeDing, 3000);
+// Solution : The problem with this code was that the function writeDing was being invoked inside the setTimeout method which did not allow the setTimout method to work properly.
+
+
